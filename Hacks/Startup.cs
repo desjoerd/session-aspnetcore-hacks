e52a153da041a3f40852d2a7fa7ee30a52f6618f
desjoerd <sjoerd.meer@outlook.com>
2019-02-18 21:42:41 +0100

    Add Swashbuckle

--- Hacks/Startup.cs
+++ Hacks/Startup.cs
@@ -15,6 +15,12 @@ namespace Hacks
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Hacks API", Version = "v1" });
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +30,15 @@ namespace Hacks
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hacks API");
+            });
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
---
+++ background.md
@@ -0,0 +1,4 @@
+#  Session aspnetcore hacks background info
+
+The scaffolded project uses Swashbuckle, NSwag was also an option, maybe something to look at in the future.
+
